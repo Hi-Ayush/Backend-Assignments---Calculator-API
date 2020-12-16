307bb5c3148222c94c29e5d11ce4a7b33ab8a56e
Hi-Ayush <ayush.kumar.ee20@heritageit.edu.in>
2020-12-16 21:30:43 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,130 +1,114 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
+const port = 3000;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-
-const isValidAddition=(num1,num2)=>{
-    if(num1+num2>1000000){
-        return({status: "error",message:"Overflow"});
-    }
-    else if(num1+num2<-1000000){
-        return ({status: "error",message:"Underflow"});
-    }
-    else{
-      return ( {
-            status: "success",
-            message: "the sum of given two numbers",
-            sum: num1+num2
-            });
-    }
-}
-const isValidSubtraction=(num1,num2)=>{
-    if(num1-num2>1000000){
-        return({status: "error",message:"Overflow"});  
-    }
-    else if(num1-num2<-1000000){
-        return ({status: "error",message:"Underflow"});
-    }
-    else{
-        return ({
-            status:"success",
-            message:"the difference of given two numbers",
-            difference: num1-num2
-        });
-    }
-}
-const isValidMultiplication=(num1,num2)=>{
-    if(num1*num2>1000000){
-        return({status: "error",message:"Overflow"});  
-    }
-    else if(num1*num2<-1000000){
-        return ({status: "error",message:"Underflow"});
-    }
-    else {
-        return ({
-            status:"success",
-            message:"The product of given numbers",
-            result: num1*num2
-        });
-    }
-}
-
-const isValidDivision=(num1,num2)=>{
-    if(num2===0){
-        return ({status: "error",message:"Cannot divide by zero"});
-    }
-    else{
-        return ({
-            status:"success",
-            message:"The division of given numbers",
-            result: num1/num2
-        });
-    }
-}
-
-const isValidInput=(num1,num2,operation)=>{
-
-    num1=parseFloat(num1);
-    num2=parseFloat(num2);
-    
-    if(typeof(num1)==="string"||typeof(num2)==="string"){
-        
-        return ({status: "error",message:"Invalid data types"});
-    }
-
- 
-    if(num1<-1000000||num2<-1000000){
-        return ({status: "error",message:"Underflow"});
-    }
-    if(num1>1000000||num2>1000000){
-        return({status: "error",message:"Overflow"});
-    }
-   
-    if(operation==="add"){
-        return isValidAddition(num1,num2);
-        
+const isValidAddition = (num1, num2) => {
+  if (num1 + num2 > 1000000) {
+    return { status: "error", message: "Overflow" };
+  } else if (num1 + num2 < -1000000) {
+    return { status: "error", message: "Underflow" };
+  } else {
+    return {
+      status: "success",
+      message: "the sum of given two numbers",
+      sum: num1 + num2
     };
-    if(operation==="dif"){
-        return  isValidSubtraction(num1,num2);
-        
-    }
-    if(operation==="mul"){
-        return isValidMultiplication(num1,num2);
-        
-    }
-    if(operation==="div"){
-        return  isValidDivision(num1,num2);
-    
-    }
-}
+  }
+};
+const isValidSubtraction = (num1, num2) => {
+  if (num1 - num2 > 1000000) {
+    return { status: "error", message: "Overflow" };
+  } else if (num1 - num2 < -1000000) {
+    return { status: "error", message: "Underflow" };
+  } else {
+    return {
+      status: "success",
+      message: "the difference of given two numbers",
+      difference: num1 - num2
+    };
+  }
+};
+const isValidMultiplication = (num1, num2) => {
+  if (num1 * num2 > 1000000) {
+    return { status: "error", message: "Overflow" };
+  } else if (num1 * num2 < -1000000) {
+    return { status: "error", message: "Underflow" };
+  } else {
+    return {
+      status: "success",
+      message: "The product of given numbers",
+      result: num1 * num2
+    };
+  }
+};
 
-app.post("/add",(req,res)=>{
-    res.send(isValidInput(req.body.num1,req.body.num2,"add"));
-})
-app.post("/sub",(req,res)=>{
-    res.send(isValidInput(req.body.num1,req.body.num2,"dif"));
-})
-app.post("/multiply",(req,res)=>{
-    res.send(isValidInput(req.body.num1,req.body.num2,"mul"));
-})
-app.post("/divide",(req,res)=>{
-    res.send(isValidInput(req.body.num1,req.body.num2,"div"));
-})
+const isValidDivision = (num1, num2) => {
+  if (num2 === 0) {
+    return { status: "error", message: "Cannot divide by zero" };
+  } else {
+    return {
+      status: "success",
+      message: "The division of given numbers",
+      result: num1 / num2
+    };
+  }
+};
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+const isValidInput = (num1, num2, operation) => {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    return { status: "error", message: "Invalid data types" };
+  }
+
+  if (num1 < -1000000 || num2 < -1000000) {
+    return { status: "error", message: "Underflow" };
+  }
+  if (num1 > 1000000 || num2 > 1000000) {
+    return { status: "error", message: "Overflow" };
+  }
+
+  if (operation === "add") {
+    return isValidAddition(num1, num2);
+  }
+  if (operation === "dif") {
+    return isValidSubtraction(num1, num2);
+  }
+  if (operation === "mul") {
+    return isValidMultiplication(num1, num2);
+  }
+  if (operation === "div") {
+    return isValidDivision(num1, num2);
+  }
+};
+
+app.post("/add", (req, res) => {
+  res.send(isValidInput(req.body.num1, req.body.num2, "add"));
+});
+app.post("/sub", (req, res) => {
+  res.send(isValidInput(req.body.num1, req.body.num2, "dif"));
+});
+app.post("/multiply", (req, res) => {
+  res.send(isValidInput(req.body.num1, req.body.num2, "mul"));
+});
+app.post("/divide", (req, res) => {
+  res.send(isValidInput(req.body.num1, req.body.num2, "div"));
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
